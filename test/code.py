1bb0ee9c907fd41d1d5ec0b610cde3c402ef31c4
Llith <kyllian.beasse@live.fr>
2019-10-04 16:16:17 +0200

    2ième modif

--- test/code.py
+++ test/code.py
@@ -1,6 +1,12 @@
-verif = true
-if verif :
-	print("bravo super !")
-nom = 'Jhon'
-prenom = input("quel est ton prenom ?")
+def taille:
+	verif = true
+	if verif :
+		print("bravo super !")
+	nom = 'Jhon'
+	prenom = input("quel est ton prenom ?")
+	if len(prenom) >7:
+		print("beaucoup de lettres")
+	else:
+		print("prenom rapide a ecrire")
+	return verif
 
